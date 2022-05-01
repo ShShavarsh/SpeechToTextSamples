4789d93986f6e020b035c2d706ee7394b23a5a36
Shavarsh Shahoyan <shavarsh.sh@gmail.com>
2022-05-01 19:20:18 +0400

    changed project structure

---
+++ SpeechToText/sampleSpeechtoText/sphinxOfflineSpeechToText.py
@@ -0,0 +1,28 @@
+
+
+#pip install pipwin
+#pipwin install pyaudio
+#pipwin install pocketsphinx
+
+import os
+import speech_recognition as sr
+
+command2mp3 = "ffmpeg -i C:\\Users\\Shavarsh\\MyProjects\\BuildUP\\Projects\\DesignReview.mp4 C:\\Users\\Shavarsh\\MyProjects\\BuildUP\\Projects\\DesignReview.mp3"
+command2wav = "ffmpeg -i C:\\Users\\Shavarsh\\MyProjects\\BuildUP\\Projects\\x.mp3 C:\\Users\\Shavarsh\\MyProjects\\BuildUP\\Projects\\x.wav"
+c5 = "C:\\Users\\Shavarsh\\MyProjects\\BuildUP\\Projects\\x.wav"
+
+os.system(command2wav)
+
+r = sr.Recognizer()
+with sr.AudioFile(c5) as source:
+    audio = r.record(source,duration=10)
+
+sFinalResult = r.recognize_sphinx(audio,language = 'en-US')#, show_all = True
+print(sFinalResult)
+
+
+
+
+
+
+
